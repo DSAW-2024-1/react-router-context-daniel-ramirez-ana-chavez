32a1a07379ebbbd9394486157758f507d6a6bf64
Santii08 <dan.santiago.rc2005@gmail.com>
2024-04-28 18:45:17 -0500

    1.0.0

---
+++ src/Components/Overview.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Overview = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Esta es la página "Acerca de" de nuestra aplicación.</p>
+    </div>
+  );
+};
+
+export default Overview;
