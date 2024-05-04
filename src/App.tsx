29658defc95dfdaac81f46a6719556994e4c1e2c
Santii08 <dan.santiago.rc2005@gmail.com>
2024-05-04 17:18:04 -0500

    1.1.9

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import "./App.css";
-import Home from "./Components/Home";
-import Contact from "./Components/Contact";
-import Overview from "./Components/Overview";
-import Login from "./Components/Login";
-import { Route, Routes } from "react-router-dom";
 
 function App() {
   return <h1>hola</h1>;
